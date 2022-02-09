ea0abb871902a1f1606959bfd6e6d566b6c56d3c
Pavel Lebedev <lebedpavel.dev@gmail.com>
2022-02-09 15:17:32 +0300

    feat: end task- brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openBrackets = [];
+  const closeBrackets = {};
+  const stack = [];
+  bracketsConfig.forEach(el => {
+    openBrackets.push(el[0]);
+    closeBrackets[el[1]] = el[0];
+  });
+  for (let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+    let topElement = stack[stack.length - 1];
+    if (openBrackets.includes(currentSymbol)) {
+      stack.push(currentSymbol);
+      if (currentSymbol === closeBrackets[currentSymbol] && closeBrackets[currentSymbol] === topElement) {
+        stack.pop();
+        stack.pop();
+      }
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+      if (closeBrackets[currentSymbol] === topElement) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
 }
